3e93038f68a824c1c6a33912d15b496399801571
mrugankray <mrugankray2@gmail.com>
2018-03-20 01:57:18 +0530

    linked db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
     //database:'mongodb://localhost:27017/meanauth',
-    database:'mongodb://mrugankray:70081193130Rant@@ds237858.mlab.com:37858/meanauthapp',
+    database:'mongodb://mrugankray:1234@ds237858.mlab.com:37858/meanauthapp',
     secret:'mysecret'
 }
